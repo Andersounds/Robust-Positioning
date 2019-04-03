4f16bb67ae4a05df4bf144077d555729f3cbf9cf
Andersound <fa8@kth.se>
2019-04-03 10:18:18 +0200

    Changed structure so that new RoIs are found for all state 0-anchors at the same time. Also make use of the new getState, setState, getZeroStateIndexes methods

--- spike/ORBTrackingSwitch.cpp
+++ spike/ORBTrackingSwitch.cpp
@@ -16,7 +16,6 @@ Problem:
 */
 int main(int argc, char** argv){
 //Settings
-    int noOfClusters = 1;
     int clusterSize = 70;
     int minDistance = 100;
     int roiFeatures = 200;
@@ -30,8 +29,9 @@ int main(int argc, char** argv){
     std::vector<trackedObject> anchors;             // Anchors tracked
     int totlaps=0;                                  // Counter of every frame. Can theoretically overflow
     int state = 0;                                  // State holder for Switch statement
+    bool getNewRegions = true;                      // A persistent variable that dictates if the inverted mask should be calculated. Basically just to save time
     to::trackedObjectList objectList(noOfAnchors,noOfParallell);  // List containing all tracked general anchors
-    std::vector<int> stateSequence{2,1,0};// In which order to process all active anchors (based on state)
+    std::vector<int> stateSequence{2,1,0};          // In which order to process all active anchors (based on state)
 //Initialize objects
     //Clock
     clock_t tStart = clock();
@@ -58,7 +58,8 @@ while(1){
 
     for(int substate:stateSequence){//All active anchors need to be processed in an order depending on their state
         for(int currentAnchor = 0;currentAnchor<objectList.no_of_parallel;currentAnchor++){//Loop through all indexes of active anchors
-            if(objectList.activeStates[currentAnchor] != substate){continue;}//Only run the switch statement if the state of current anchor is correct
+            state = objectList.getState(currentAnchor);//Take the current state of the current active anchor
+            if(state != substate){continue;}//Only run the switch statement if the state of current anchor is correct
 //Reinitialize some temporary vectors on the stack
             std::vector< cv::KeyPoint > keypoints;
             std::vector< cv::KeyPoint > roIKeypoints;
@@ -67,19 +68,25 @@ while(1){
             cv::Mat sceneDescriptors;
             cv::Mat objectDescriptors;
 
-            state = objectList.activeStates[currentAnchor];//Take the current state of the current active anchor
             switch (state) {
-                case 0:{//Get new regions to track
+                case 0:{//Get new regions to track for all anchors with state 0
+                    std::vector<int> zeroIndexes = objectList.getZeroStateIndexes();//Get indexes of all active anchors with state 0
+                    int noOfClusters = zeroIndexes.size();
                     tracker.getFeatures(frame,keypoints);//Detect ORBs in whole scene
                     rectangles = tracker.findClusters(frame,invertedMask,keypoints, noOfClusters, clusterSize, minDistance);//Get cluster
-                    tracker.getMask(rectangles[0],roIMask);
-                    roIKeypoints = roiTracker.getFeatures(frame,roIMask);  //Get new features only within cluster rectangle
-                    cv::KeyPoint::convert(roIKeypoints, roIPoints);     //Convert to Points to be used by KLT-tracker
-                    if(roIKeypoints.size()>1&&keypoints.size()>1){      //Must be at least one pair
-                        roiTracker.orbObject->compute(frame, roIKeypoints, objectDescriptors);//Calculate the keypoint descriptors from the current frame
-                        objectList.add(new trackedObject(roIKeypoints, objectDescriptors,rectangles[0]));//Add new region to list of tracked objects
+                    //Repeated for all rectangles
+                    for(int index =0;index<noOfClusters;index++){
+                        tracker.getMask(rectangles[index],roIMask);
+                        roIKeypoints = roiTracker.getFeatures(frame,roIMask);  //Get new features only within cluster rectangle
+                        cv::KeyPoint::convert(roIKeypoints, roIPoints);     //Convert to Points to be used by KLT-tracker
+                        if(roIKeypoints.size()>1&&keypoints.size()>1){      //Must be at least one pair
+                            roiTracker.orbObject->compute(frame, roIKeypoints, objectDescriptors);//Calculate the keypoint descriptors from the current frame
+                            int objectID = objectList.add(new trackedObject(roIKeypoints, objectDescriptors,rectangles[index]));//Add new region to list of tracked objects
+                            //objectList.assign(currentAnchor,objectID)
+                            objectList.setState(zeroIndexes[index],2);//Do KLT tracking next
+                        }
                     }
-                state = 2; //Do KLT tracking next
+                //state = 2; //Do KLT tracking next
                 break;
                 }
                 case 1:{//Locate tracked objects in new frame
@@ -88,43 +95,54 @@ while(1){
                     int index = 0;
                     for(int index=0;index < objectList.no_of_tracked;index++){//Try to match with all tracked objects (that are not active) until match found
                         int anchorID = objectList.list[index]->ID;
-                        //If the current object is already active, then dont try to match with it
-                        if(objectList.activeIDs.find(ID)){continue;}
+                        if(objectList.isActive(anchorID)){continue;}//If the current object is already active, then dont try to match with it
                         matcherObject ->match(objectList.list[index]->originalDescriptors,sceneDescriptors,matches);//Perform matching
                         hd::getGoodMatches(matches, good_matches);//Sort out the best 30%
                         std::vector<cv::Point> xWinningPairs, yWinningPairs;
                         std::vector<float> status;
                         // Do Hough detection
                         status = hd::detect(good_matches, objectList.list[index]->originalKeyPoints,keypoints, xWinningPairs, yWinningPairs);
-
                         if(status[0]){//Match!
                             std::cout << index << std::endl;
                             std::vector<cv::DMatch> winningMatches = hd::getBestMatches(good_matches,xWinningPairs,yWinningPairs);
                             roIPoints.clear(); //Clear vector to remove garbage data
                             //
                             roIPoints = objectList.list[index]->getTrackablePoints(status[1],status[2],winningMatches,keypoints,rectangles[0]);
-                            //posibly only black out if there is at least one active anchor in state 0. Otherwise its not needed.
-                            cv::rectangle(invertedMask,rectangles[0],1,CV_FILLED,cv::LINE_8,0);// Black out the inverted mask where the anchor is found
-                            state = 2;//Do KLT tracking next
+                            if(getNewRegions){//Only black out if there is at least one active anchor in state 0. Otherwise its not needed.
+                                cv::rectangle(invertedMask,rectangles[0],1,CV_FILLED,cv::LINE_8,0);// Black out the inverted mask where the anchor is found
+                            }
+                            objectList.setState(currentAnchor,2);//Do KLT tracking next
+                            //objectList.assignAnchor(currentAnchor, index)
+                            //objectList.update(currentAnchor, rectangle, roIPoints)
+                            //state = 2;//Do KLT tracking nex
                             break;// Break after first match is found
                         }
                         else{std::cout << "-";}//Not match
                     }
-                if(state==1){state=0;std::cout << std::endl;}//If no match has been found, go to state 0 next. Print endline only also to show in terminal
+                if(objectList.getState(currentAnchor)==1){               //If no match has been found
+                    objectList.setState(currentAnchor,0);                //The anchor goes into state 0
+                    getNewRegions = true;   //At least one new region needs to be found next lap
+                    std::cout << std::endl; //Print endline to show in terminal
+                }
                 break;
                 }
                 case 2:{//KLT track points from prev image in new image
                     int success = tracker.trackOpticalFlow(prevFrame,frame,roIPoints,rectangles[0]); //Do KLT tracking
-                    //posibly only black out if there is at least one active anchor in state 0. Otherwise its not needed.
-                    cv::rectangle(invertedMask,rectangles[0],1,CV_FILLED,cv::LINE_8,0);// Black out the inverted mask where the anchor is found
-                    if(!success){//If tracking was not successful
-                        // Remove the anchor from list of active anchors.
-                        state = 1; //Go to state 1
+                    if(getNewRegions){//Only black out if there is at least one active anchor in state 0. Otherwise its not needed.
+                        cv::rectangle(invertedMask,rectangles[0],1,CV_FILLED,cv::LINE_8,0);// Black out the inverted mask where the anchor is found
+                    }
+                    if(success){//If tracking was successful
+                        int ghjj = 0; //Here we should update the rect and points of the tracked anchor
+                        //objectList.update(currentanchor,rect,points)
+                    }
+                    else{//If not successful
+                        objectList.setState(currentAnchor,1);//Go to state 1
+                        //state = 1; //Go to state 1
                     }
                 break;
                 }
             }//End of switch statement
-            objectList.activeStates[currentAnchor] = state;//Set the new state of the current active anchor
+            //objectList.activeStates[currentAnchor] = state;//Set the new state of the current active anchor
         }//End of for loop
     }//End of sequence loop
     tracker.drawPoints(mat4visual,roIPoints,mat4visual,CV_RGB(255,0,0));
