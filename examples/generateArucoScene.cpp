bd7b8fd474c3b434e472718d46e65eb60e82c998
Andersound <fa8@kth.se>
2019-05-07 09:11:23 +0200

    Initial commit of function to generate aruco simulation scene

---
+++ examples/generateArucoScene.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <opencv2/opencv.hpp>
+#include <opencv2/aruco.hpp>
+/*
+This file is to be used to generate a 2d floor environment with ArUco markers to be used
+for triangulation evaluation
+*/
+
+int main(int argc, char** argv){
+
+
+
+
+
+}
