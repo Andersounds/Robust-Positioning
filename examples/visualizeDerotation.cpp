674e08419c6b5169e40a3ced10e6921da0392f00
Andersound <fa8@kth.se>
2019-05-27 17:10:08 +0200

    Changed the code so it supports the new optical flow class with combined KLT and COrr

--- examples/visualizeDerotation.cpp
+++ examples/visualizeDerotation.cpp
@@ -3,39 +3,33 @@
 
 //#include "../src/simulatePose.cpp"
 
-#include "../src/KLTFlowField.cpp"
+
 #include "../src/simulatePose.hpp"
 #include "../src/homographyVO.hpp"
 #include "../src/save2file.cpp"
+#include "../src/opticalFlow.hpp"
 
-#include "../src/corrFlowField.cpp"
 
 #define PI 3.14159
-/*
-This code is a basic stream of sim chessboard warped images that
-also extracts and draws the flowfield
-*/
 
 /*
-Issue:
-VO algorithm performs MUCH better when tracked features are simply thrown away every frame.
-    Is there a bug? can I handle it somehow?
-
-Standardizer order of roll/pitch?
+TODO: Standardizer order of roll/pitch?
 
 */
-
 
 /* Draws arrows between the point correspondances and scale them
  *
  */
-void drawArrows(cv::Mat img,cv::Mat& outputImg,std::vector<cv::Point2f> features1,std::vector<cv::Point2f> features2,float scale, cv::Point2f offset){
+void drawArrows(cv::Mat& outputImg,std::vector<cv::Point2f> features1,std::vector<cv::Point2f> features2,float scale, cv::Point2f offset){
     std::vector<cv::Point2f>::iterator it1 = features1.begin();
     std::vector<cv::Point2f>::iterator it2 = features2.begin();
     while(it1!=features1.end()){
+
         cv::Point2f from = *it1 + offset;
         cv::Point2f to = (*it2 - *it1)*scale + *it1 + offset;
         cv::arrowedLine(outputImg,from,to,CV_RGB(200,50,0),2,cv::LINE_8,0,0.1);
+        //cv::imshow("Chess board", outputImg);
+        //cv::waitKey(0);
         it1++;
         it2++;
     }
@@ -49,6 +43,8 @@ void drawFeatures(cv::Mat inputImg, std::vector<cv::Point2f> features, cv::Mat& 
     cv::drawKeypoints(inputImg, kp, outputImg,color);
 
 }
+/* Draws a closed loop between all given points
+ */
 void drawLines(cv::Mat& img,std::vector<cv::Point2f> points,cv::Point2f offset){
     int size = points.size();
     int i = 0;
@@ -89,33 +85,23 @@ int main(void){
     std::ofstream file_estimated;
 // Init simulation environment
     simulatePose warper;
-
-    warper.setBaseScene(boxWidth,rowsOfBoxes,colsOfBoxes);
-//cv::Mat floor = cv::imread("/Users/Fredrik/Datasets/FloorTextures/light_stone_wall.jpg",cv::IMREAD_COLOR);
-//cv::Mat floor = cv::imread("/Users/Fredrik/Datasets/FloorTextures/test1.png",cv::IMREAD_COLOR);
-/*cv::Mat floor = cv::imread("/Users/Fredrik/Datasets/FloorTextures/test2.png",cv::IMREAD_REDUCED_COLOR_4);
+//    warper.setBaseScene(boxWidth,rowsOfBoxes,colsOfBoxes);
+cv::Mat floor = cv::imread("/Users/Fredrik/Datasets/FloorTextures/test2.png",cv::IMREAD_REDUCED_COLOR_4);
+    if(floor.empty()){std::cout << "Could not read file" << std::endl;return 0;}
     cv::Mat floor8U;
     cv::cvtColor(floor, floor8U, cv::COLOR_BGR2GRAY);
     warper.setBaseScene(floor);
-*/
+
 
 //    cv::imshow("Chess board",floor);
 //    cv::waitKey(0);
 
-
     warper.setParam("d",2);             //Camera in base pose is 1 m from scene
     warper.setParam("sceneWidth",4);    //Scenewidth is 2m
     warper.setParam("yaw",-3.1415/2);   // Camera is rotated 90 deg cc in base pose
-
     warper.setParam("x",2);         //Set global origin at (-x,-y) from basepose
     warper.setParam("y",0.7);
-
     warper.init(0);//Initialize with configuration 0
-
-
-//Create path of camera and save to output file
-
-    //Start out aligned with x-y of global coordinate system
 
 /*    float length = 200;
     std::vector<float> xPath = linSpace(2,2,length);
@@ -135,7 +121,6 @@ int main(void){
     float pathScale = 1;
     std::vector<float>::iterator xIt = xPath.begin();
     std::vector<float>::iterator yIt = yPath.begin();
-
     while(xIt != xPath.end()){
             *xIt*=pathScale;
             *yIt*=pathScale;
@@ -143,18 +128,16 @@ int main(void){
             yIt++;
     }
 
-    std::vector<float> zPath = linSpace(-0.8,-0.7,length);
+    std::vector<float> zPath = linSpace(-0.8,-0.8,length);
 
 
     file_true.open("truePath.txt", std::ios::out | std::ios::app);
     std::vector<std::vector <float>> input{xPath,yPath,zPath,yawPath};
     build_path(input,file_true);
     file_true.close();
-
 //Init flowField object with default settings
-    featureVO FlowField;
-    FlowField.setFeatureDefaultSettings();
-    FlowField.setKLTDefaultSettings();
+    of::opticalFlow FlowField(USE_KLT,3,150);
+    FlowField.setDefaultSettings();
 //Init odometry object
     cv::Mat_<float> K;
     warper.K.copyTo(K);//Can not assign with = as they then refer to same object. edit one edits the other
@@ -165,7 +148,6 @@ int main(void){
     vo::planarHomographyVO odometer(K,T,USE_AFFINETRANSFORM); //USE_HOMOGRAPHY
     //odometer.activateDerotation = false;//Deactivate derotation. is it needed even?
     //Initial values of UAV position
-    //cv::Mat_<float> R = warper.getZRot(yawPath[0]);
     cv::Mat_<float> R = warper.getZRot(0);
     cv::Mat_<float> t = cv::Mat_<float>::zeros(3,1);
     t(0,0) = xPath[0];
@@ -175,80 +157,49 @@ int main(void){
 
 //Set some parameters that are used trhoughout the program
 //and initialize some variables on bottom of stack
-    std::vector<cv::Point2f> features; //The vector that keeps all the current features
-    int noOfCorners = 20;
-    int noOfTracked = 0;//Init
-    float maskDir = -1;//to choose mask. init value -1 for whole scene
+    //std::vector<cv::Point2f> features; //The vector that keeps all the current features
     int cols = warper.baseScene.cols;//boxWidth*colsOfBoxes;
     int rows = warper.baseScene.rows;//boxWidth*rowsOfBoxes;
-    cv::Rect_<float> focusArea = FlowField.getFocusArea(cols,rows,150,150);//(cols,rows,size,size)
-    //focusArea.x+=100;focusArea.y+=40;
+// TODO --- an easier way of setting the focusarea. should only define it once
+
+    float x_ = (float) cols/2 - 75;
+    float y_ = (float) rows/2 - 75;
+    cv::Rect_<float> focusArea(x_,y_,150,150);
     cv::Point2f focusOffset(focusArea.x,focusArea.y);
+
     cv::Mat subPrevFrame;//For finding new corners
+    cv::Mat colorFrame;//For illustration
 
-
-cv::Mat colorFrame;//For illustration
-
-
-    corrFlowField corrTracker;
 //Go through whole path
     for(int i=0;i<(int)length;i++){
 //Get new image
-        float pitch = 0;
-        float height = abs(zPath[i])/(cos(pitchPath[i])*cos(rollPath[i]));//Används bara av odometer
         std::vector<float> trueCoordinate{xPath[i],yPath[i],zPath[i]};
-
-        //std::cout << "Coordinates true: "<< trueCoordinate[0] <<", " << trueCoordinate[1] << ", "<< trueCoordinate[2] << std::endl;
-        //std::cout << "Coordinates est init: "<< t << std::endl;
         std::vector<float> angles{yawPath[i],pitchPath[i],rollPath[i]};
-//        cv::Mat rawFrame = warper.getWarpedImage(angles,trueCoordinate);
+
         cv::Mat rawFrame = warper.uav2BasePose(angles,trueCoordinate);
+
         cv::Mat frame;
         cv::cvtColor(rawFrame, frame, cv::COLOR_BGR2GRAY);
+
 //Process image...
         cv::Mat subFrame = frame(focusArea);
-////////
 
-
-        if(noOfTracked<=7){//if(noOfTracked<=noOfCorners*0.7){
-            int noOfNew = noOfCorners-noOfTracked;
-            std::vector<cv::Point2f> newFeatures;
-            cv::Point2f dir(0,0);
-            cv::Rect_<float> dirMask = FlowField.getDirMask(focusArea,dir);
-            //CHOOSE SUBSET HERE WITH DIRMASK
-            //subPrevFrame(dirMask) put this in getCorners
-            FlowField.getCorners(subPrevFrame, newFeatures,noOfNew);//,dirMask);
-            FlowField.conCat(features,newFeatures);//Add the new features to old vector
-        }
-
+        //Get flow field
+        std::vector<cv::Point2f> features;
         std::vector<cv::Point2f> updatedFeatures; //The new positions estimated from KLT
-        std::vector<uchar> status;
-        std::vector<float> errors;
-        FlowField.trackKLT(subPrevFrame,subFrame,features,updatedFeatures,status,errors);
-        //Get just the correspondance pairs that are active. AND SHIFT TO IMAGE COORD
-        std::vector<cv::Point2f> activeFeatures1,activeFeatures2;
-        noOfTracked = FlowField.extractActiveFeatures(features,status,activeFeatures1,focusOffset);
-        FlowField.extractActiveFeatures(updatedFeatures,status,activeFeatures2,focusOffset);
+        FlowField.getFlow(subPrevFrame,subFrame,features,updatedFeatures);
+
+        //Save flow field for plotting. De-rotation will scale it down so it wont be visible
+        std::vector<cv::Point2f> p1_original = features;
+        std::vector<cv::Point2f> p2_original = updatedFeatures;
+        //VO estimation
+        float height = abs(zPath[i])/(cos(pitchPath[i])*cos(rollPath[i]));//Används bara av odometer
+        bool odometerSuccess = odometer.process(features,updatedFeatures,rollPath[i],pitchPath[i],height,t,R);
 
 
-/////////
 
-/////////
-/*
-std::vector<cv::Point2f> activeFeatures1;
-std::vector<cv::Point2f> activeFeatures2;
-corrTracker.corrFlow(subPrevFrame,subFrame,activeFeatures1,activeFeatures2);
-*/
-//////////
-        std::vector<cv::Point2f> p1_original;
-        std::vector<cv::Point2f> p2_original;
-        for(int i=0;i<noOfTracked;i++){
-            p1_original.push_back(activeFeatures1[i]);
-            p2_original.push_back(activeFeatures2[i]);
-        }
 
-        bool odometerSuccess = odometer.process(activeFeatures1,activeFeatures2,rollPath[i],pitchPath[i],height,t,R);
-        float z_sin = R(0,1);
+
         float zAngle_sin = std::atan2(R(0,1),R(0,0));//tan with quadrant checking
 
 
@@ -263,26 +214,25 @@ corrTracker.corrFlow(subPrevFrame,subFrame,activeFeatures1,activeFeatures2);
         //Illustrate
         //Simulation image
         rawFrame.copyTo(colorFrame);
-        //cv::cvtColor(frame, colorFrame, cv::COLOR_GRAY2BGR);
         float scale = 10;
-        drawArrows(colorFrame,colorFrame,p1_original,p2_original,scale,focusOffset);
-        //drawFeatures(colorFrame, features, colorFrame, CV_RGB(2,200,0));
+        drawArrows(colorFrame,p1_original,p2_original,scale,focusOffset);
         cv::rectangle(colorFrame,focusArea,CV_RGB(255,0,0),2,cv::LINE_8,0);
         cv::putText(colorFrame, "Original view and flow field",cv::Point(20, 20),cv::FONT_HERSHEY_DUPLEX,0.7,CV_RGB(118, 185, 0),2);
-
         //Straight image
         std::vector<cv::Point2f> roiCorners{cv::Point2f(0,0),cv::Point2f(150,0),cv::Point2f(150,150),cv::Point2f(0,150)};
-        float pitch__ = angles[2];
-        float roll__ = 0;
         odometer.deRotateFlowField(roiCorners, rollPath[i],pitchPath[i]);
-        angles[2]=0;
+        //Set roll and pitch to zero. ORDER????
+        angles[1] = 0;
+        angles[2] = 0;
         cv::Mat straightFrame = warper.uav2BasePose(angles,trueCoordinate);
-        drawArrows(straightFrame,straightFrame,activeFeatures1,activeFeatures2,scale,focusOffset);
+        odometer.deRotateFlowField(features, rollPath[i],pitchPath[i]);
+        odometer.deRotateFlowField(updatedFeatures, rollPath[i],pitchPath[i]);
+        drawArrows(straightFrame,features,updatedFeatures,scale,focusOffset);
         cv::putText(straightFrame, "No-pitch view with derotated flow field",cv::Point(20, 20),cv::FONT_HERSHEY_DUPLEX,0.7,CV_RGB(118, 185, 0),2);
         drawLines(straightFrame,roiCorners,focusOffset);
-
         //Create new image with both scenes
-        cv::Mat both(cv::Size(colorFrame.rows*2,colorFrame.cols),colorFrame.type(),cv::Scalar(0));
+        ////cv::Mat both(cv::Size(colorFrame.rows*2,colorFrame.cols),colorFrame.type(),cv::Scalar(0));
+        cv::Mat both(cv::Size(colorFrame.cols,colorFrame.rows*2),colorFrame.type(),cv::Scalar(0));
         cv::Mat roi1 = both(cv::Rect(0,0,colorFrame.cols,colorFrame.rows));
         colorFrame.copyTo(roi1);
         cv::Mat roi2 = both(cv::Rect(0,colorFrame.rows,colorFrame.cols,colorFrame.rows));
@@ -295,15 +245,6 @@ corrTracker.corrFlow(subPrevFrame,subFrame,activeFeatures1,activeFeatures2);
         }
         if( cv::waitKey(1) == 27 ) {std::cout << "Bryter"<< std::endl;return 1;}
         //cv::waitKey(0);
-
-
-
-
-        //Time-shift frames and features
-        //features = activeFeatures2;
-
-        //features.clear(); // These two lines are here because the algorithm performs much better when features are just thorwn away and found again
-        //noOfTracked = 0;  // WHen timing manually it is actually  a tiny bit faster. probably because the copying takes time.
 
         subFrame.copyTo(subPrevFrame);//Could just shift prevframe and subprevframe would be automatically shifted?
     }
