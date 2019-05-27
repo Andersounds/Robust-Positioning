bde408e2e1ca5a01a20c995ab6f27c1e4a2fa2d3
Andersound <fa8@kth.se>
2019-05-27 15:12:54 +0200

    Implemented phase-correlation optical flow in framework

--- src/opticalFlow.cpp
+++ src/opticalFlow.cpp
@@ -1,3 +1,120 @@
 #include <iostream>
 #include <opencv2/opencv.hpp>
 #include "opticalFlow.hpp"
+
+
+
+
+/* Constructor. Calculates some sizes such as actual grid size
+ *
+ */
+of::opticalFlow::opticalFlow(int mode_, int grid_, int roiSize){
+    mode = mode_;
+    grid = grid_;
+    //Calculate integer distance between each flow arrow
+    h = floor( (float)roiSize / (float) grid );
+    grid_x = floor(( ((float)roiSize) - h *((float)grid) + h)/2); //Calculate center (integer) of first roi
+    grid_y = grid_x;
+
+    //Define p1 vector to be used in each function call
+    std::vector<cv::Point2f> p1_;
+    float y = grid_y;
+    for(int i=0;i<grid;i++){
+        float x = grid_x;
+        for(int j=0;j<grid;j++){
+            p1_.push_back(cv::Point2f(x,y));
+            x += h;
+
+        }
+        y += h;
+
+    }
+
+    //Assign p1 corresponcence points
+    p1 = p1_;
+    //Init parameters for the chosen optical flow method
+    if(mode == USE_KLT){
+
+
+    }else if(mode == USE_CORR){
+
+        thresh = 0;
+        //Define subRoI vector
+        std::vector<cv::Rect2f> subRoI_;
+        float h_half = floor( h/2 );
+        for(int i=0;i<p1.size();i++){
+            float x = p1[i].x - h_half;
+            float y = p1[i].y - h_half;
+            subRoI_.push_back(cv::Rect2f(x,y,h,h));
+        }
+        subRoI = subRoI_;
+        //Create hanning window to reduce edge effects in phase correlation calculation
+        cv::Mat hann_;
+        cv::createHanningWindow(hann_,cv::Size(h,h), CV_32FC1);
+        hann_.copyTo(hann);
+    } else{
+        std::cout << "Wrong optical flow mode (opticalFlow::opticalFlow)" << std::endl;
+        //cv::error();
+    }
+}
+/* Used to set default settings for the optical flow algorithms
+ *
+ */
+void of::opticalFlow::setDefaultSettings(void){
+
+
+}
+/* Wrapper function to calculate the optical flow. Calles either KLT or Corr depending on mode
+ *
+ */
+bool of::opticalFlow::getFlow(const cv::Mat& src1, const cv::Mat& src2,
+                std::vector<cv::Point2f>& points1,std::vector<cv::Point2f>& points2){
+    if(mode == USE_KLT){
+        return false;
+    }else if(mode == USE_CORR){
+        return corrFlow(src1,src2,points1,points2);
+    } else{
+        std::cout << "Invalid mode (of::opticalFlow::getFlow)" << std::endl;
+        return false;
+    }
+}
+
+/* This method implements a variant of Walter18:s phase correlation optical flow
+ *
+ */
+bool of::opticalFlow::corrFlow(const cv::Mat& src1, const cv::Mat& src2,
+                std::vector<cv::Point2f>& points1,std::vector<cv::Point2f>& points2){
+    if(src1.rows<1 || src1.cols<1 || src2.rows<1 || src2.cols<1){return false;}//Assert that we actually have two RoI images
+    //Assert mat type here?
+    //Make sure that they are empty
+    points1.clear();
+    points2.clear();
+    //Convert mats to float
+    cv::Mat src1_32C1;
+    cv::Mat src2_32C1;
+    double scale = 2048;//Not fully scaled, but somewhere inbetween in order to avoid rounding errors
+    double offset = 0;
+    src1.convertTo(src1_32C1,CV_32FC1,scale,offset);
+    src2.convertTo(src2_32C1,CV_32FC1,scale,offset);
+
+    std::vector<cv::Point2f>::iterator cntr = p1.begin();
+    for(cv::Rect2f roi:subRoI){
+        double response;
+//std::cout << "is sub roi converted correctly?" << std::endl;
+//cv::imshow("Chess board", src1_32C1);
+//cv::waitKey(0);
+        cv::Point2d flow = cv::phaseCorrelate(src1_32C1(roi), src2_32C1(roi),hann,&response);//Perform phase correlation calculation
+        if(response<thresh){
+            std::cout << "Too low threshold (of::opticalFlow::corrFlow)" <<std::endl;
+            points1.clear();//Clear them so that VO does not process trash data
+            points2.clear();
+            return false;
+        }
+        points1.push_back(*cntr);
+        cv::Point2f flow32 = cv::Point2f((float)flow.x,(float)flow.y) + *cntr;
+
+        points2.push_back(flow32);
+        cntr++;
+    }
+    return true;
+}
--- src/opticalFlow.hpp
+++ src/opticalFlow.hpp
@@ -12,12 +12,34 @@ Method 1. Lukas Kanade pyramidal optical flow. Without corners. an x-by-x grid o
     key: USE_KLT
 Method 2. Phase-correlation based optical flow. The RoI is divided into x-by-x squares. Each of these are phase correlated
     with the same square from the previous image, and from each square a single flow arrow is obtained
+    This method is a variant of the one presented by Walter18
     key: USE_CORR
 */
 namespace of{
-    #define USE_HOMOGRAPHY 1
-    #define USE_AFFINETRANSFORM 2
+    #define USE_KLT 1
+    #define USE_CORR 2
+    class opticalFlow{
+    public:
+        opticalFlow(int,int,int);
+        bool getFlow(const cv::Mat&, const cv::Mat&,std::vector<cv::Point2f>&,std::vector<cv::Point2f>&);
+        void setDefaultSettings(void);
+    private:
+        int mode; //Either USE_CORR or USE_KLT
+        int grid; //specifies size of flow grid (number of flow arrows in x and y direction). Total flow field will be grid^2 points arranged in a grid
+        float h; //The distance between each flow arrow in x-and y direction. Also the width of each sub roi
+        float grid_x; //x-ccordinate of first point in the used RoI.
+        float grid_y; //See above
+        std::vector<cv::Point2f> p1; //First points in point correspondances (In grid shape)
+        //Parameters for phase correlation flow field
+        double thresh;
+        std::vector<cv::Rect2f> subRoI; //Coordinates of all subRects that are used for phase-correlation
+        cv::Mat hann; //Hanning window to reduce edge effects in phase correlation
+        bool corrFlow(const cv::Mat&, const cv::Mat&,std::vector<cv::Point2f>&,std::vector<cv::Point2f>&);
 
+        //Parameters for KLT pyramidal flow field
+
+
+    };
 
 
 
