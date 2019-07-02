5bb1a30e99d02ffa567ef95272185a6173afca24
Andersound <fa8@kth.se>
2019-07-02 23:48:50 +0200

    Basic functionality of data logger

--- src/logger.cpp
+++ src/logger.cpp
@@ -1,21 +1,88 @@
 #include <iostream>
 #include <fstream> //ofstream for out, ifstream for in, fstream for both
-//#include <regex>//For syntax Check
+#include <limits> //
 #include <opencv2/opencv.hpp>
 #include "logger.hpp"
 
 log::dataLogger::dataLogger(void){
-    bufferSize = 20;
+    bufferLength = 30;
     overwrite = true;
+    sep = ',';
+
+    //Info regarding overflow
+
+    float maxint = std::numeric_limits<float>::max();
+    float hours = maxint/1000/3600;
+    std::cout << "Time until overflow of timestamp: " << hours << " hours"<< std::endl;
+    std::cout << "Consider using double-logger if overflow will occur too fast" << std::endl;
 }
 
 int log::dataLogger::init(std::string path, std::vector<std::string> dataFields){
-
+    bufferWidth = dataFields.size();
+    fullPath = path;
+    if(bufferWidth<2){
+        std::cout << "Too few datafields in dataLogger::init. Must be at least 2." <<std::endl;
+        return 0;
+    }
+    //Test open data file and if applicable, write the names of the datafields
 
     if(overwrite){
-        if(!settingsFile.open(path, std::ofstream::out | std::ofstream::trunc)) return 0;//Trunc option clears the file before anything is written
+        file.open(fullPath, std::ofstream::out | std::ofstream::trunc);//Trunc option clears the file before anything is written
+        if(!file.is_open() || !writeInfoLine(file,dataFields)){
+            std::cout << "Could not open file: \"" << path << "\". Aborting."<<std::endl;
+            return 0;
+        }
+
     } else{
-        if(!settingsFile.open(path, std::ofstream::out | std::ofstream::trunc)) return 0;
-        file_true.open(path, std::ios::out | std::ios::app);
+        file.open(fullPath, std::ofstream::out| std::fstream::ate);//Opens file to check validity of path
+        if(!file.is_open()){
+            std::cout << "Could not open file: \"" << path << "\". Aborting." << std::endl;
+            return 0;
+        }
     }
+    file.close();
+    return 1;
 }
+
+int log::dataLogger::init(std::string path, std::vector<std::string> dataFields, int bufSize, bool ovrwrite){
+    bufferLength = bufSize;
+    overwrite = ovrwrite;
+    return init(path, dataFields);
+}
+/* Writes the info line in an already open file. First element is always timestamp*/
+int log::dataLogger::writeInfoLine(std::ofstream& theFile, std::vector<std::string> dataInfo){
+    theFile << dataInfo[0];
+    for(int i=1;i<dataInfo.size();i++){
+        theFile << sep << dataInfo[i];
+    }
+    theFile << '\n';
+    return 1;
+}
+/*Method for writing to the buffer, which when it is full is dumped to the file*/
+int log::dataLogger::dump(std::vector<float>& data){
+    static int counter = 0;
+    std::vector<float>::iterator it = data.begin();
+    file.open(fullPath, std::ofstream::out | std::fstream::app);
+    file << *it;
+    it++;
+    while(it!=data.end()){
+        file << "," << *it;
+        it++;
+    }
+    file << '\n'; //Not std::endl since this flushes the buffer and we do not need it right now
+    counter++;//increment counter.
+    if(counter>bufferLength){ //If counter is high enough, make sure that buffer is flushed
+        counter = 0;
+        file.flush();
+    }
+    file.close();
+    return 1;
+}
+
+
+/*
+ * Source code for image logger
+ *
+ *
+ *
+ */
--- src/logger.hpp
+++ src/logger.hpp
@@ -1,14 +1,31 @@
+/*
+ Data logger header
+ Initial version only log float and Mat
+ i.e timestamp is also float. Use <limits> ti check max time until overflow on current system!
+ Futura meybe do double logger and see if performance is affected
+
+ ADD a destructor that flushes the buffer when object is destroyed
+*/
+
 
 
 namespace log{
 class dataLogger{
-    datalogger(void);//Constructor. sets default settings
+public:
+    std::ofstream file;
+    //cv::Mat_<float> buffer;
+    //float* buffPtr;
+    dataLogger(void);//Constructor. sets default settings
     int init(std::string,std::vector<std::string>);//init function that gives path to dir and datafield names
     int init(std::string,std::vector<std::string>,int,bool);//init function that gives path to dir, buffer size, and overwrite selection
-    int bufferSize;
+    int bufferLength;
+    int bufferWidth;
     bool overwrite;
-    std::std fullPath;
-    int dump(double,std::vector<float>);//timestamp is given as double, data is given as vector
+    char sep;
+    std::string fullPath;
+    int dump(std::vector<float>&);//timestamp is given as double, data is given as vector
+private:
+    int writeInfoLine(std::ofstream&,std::vector<std::string>);//Used to write out the first line of datafield names
 };
 class imageLogger{
     imageLogger(void);//Constructor. sets default settings
@@ -18,8 +35,8 @@ class imageLogger{
     bool overwrite;
     int initialImageNumber;
     int numOfDigits;//standard four for images img_0001, img_0002, etc.
-    std::str fullPathFile;
-    std::str imgBaseStr;
+    std::string fullPathFile;
+    std::string imgBaseStr;
     int dump(double,cv::Mat);//Timestamp is given as double, image is given as Mat
 };
 }
