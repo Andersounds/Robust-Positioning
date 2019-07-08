988af56e4e0bb82bffe0676b02fa1a144806fbba
Fred_Rpi <fa8@kth.se>
2019-07-08 12:05:10 +0100

    Some edits to make it work

--- src/videoStream_RPI.cpp
+++ src/videoStream_RPI.cpp
@@ -312,7 +312,7 @@ float robustPositioning::piCamStreamer::getImage(cv::Mat& frame){
             return -1;
         }else{
             std::cout<<"Done.\n\tConnected to camera ="<<RPICamera.getId() <<std::endl;
-            double format=RPICamera.get(CV_CAP_PROP_FORMAT)
+            double format=RPICamera.get(CV_CAP_PROP_FORMAT);
             std::cout << "Format: CV_8UC1: "<<format==CV_8UC1 <<", CV_8UC3: "<< format==CV_8UC3 << std::endl;
             std::cout << "Width="<<RPICamera.get(CV_CAP_PROP_FRAME_WIDTH)<< ", height="<<RPICamera.get(CV_CAP_PROP_FRAME_HEIGHT)<< std::endl;
             initialized = true;
