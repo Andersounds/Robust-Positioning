668fe99af094b4ce776c8bb74a09364c3fa0f29e
Andersound <fa8@kth.se>
2019-07-12 21:48:07 +0200

    Basic -but untested- example program of the i2c slave funtionality with read/write, encode/decode

---
+++ examples/basici2cSlave.cpp
@@ -0,0 +1,55 @@
+#include <pigpio.h>
+#include <iostream>
+#include <unistd.h> //For sleep
+#include "../src/i2c_slave.cpp"
+
+// give  a 1 enables. give a 0 closes gpios
+// use cin to do clean exit
+
+
+int main(int argc, char** argv){
+// Parse arguments
+    if(argc!=2){
+        std::cout << "Give -one- argument"<< std::endl;
+        std::cout << "1: enable gpio, i2c on BSC peripheral and run example" << std::endl;
+        std::cout << "0: disable gpio and i2c on BSC peripheral" << std::endl;
+        return 0;
+    }else{
+        if((argv[1] != 0) || argv[1]!=1){
+            std::cout << "Valid arguments:" << std::endl;
+            std::cout << "1: enable gpio, i2c on BSC peripheral and run example" << std::endl;
+            std::cout << "0: disable gpio and i2c on BSC peripheral" << std::endl;
+            return 0;
+        }
+    }
+    if(argv[1]==0){
+        closeSlave();
+        return 0;
+    }
+
+    //initialize i2c slave object with the inherited encode/decode class
+    robustpositioning::i2cSlave_decode i2cComm(0x03);
+    std::vector<float> values;
+    while(1){
+        if(i2cComm.readAndDecodeBuffer(values)>0){
+            std::cout << "Recieved " << i2cComm.RxBufferContentSize() <<"decoded floats" << std::endl;
+        }else{std::cout << "No available data in rx buffer" << std::endl;}
+        usleep(3000000);
+        int writtenBytes = i2cComm.writeAndEncodeBuffer(values);
+        if(writtenBytes>0){
+            std::cout << "Wrote " << writtenBytes << " bytes to tx buffer" << std::endl;
+        }else{ std::cout << "Could not write to tx buffer " << std::endl;}
+        usleep(3000000);
+    }
+
+}
+
+void closeSlave() {
+    gpioInitialise();
+    cout << "Initialized GPIOs\n";
+    xfer.control = 0;
+    bscXfer(&xfer);
+    cout << "Closed slave.\n";
+    gpioTerminate();
+    cout << "Terminated GPIOs.\n";
+}
