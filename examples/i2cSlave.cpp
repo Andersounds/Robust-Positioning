3fb066bcb51acf57baf5d02bca154f264e534575
Andersound <fa8@kth.se>
2019-07-12 11:49:59 +0200

    Added print of read buffer

--- examples/i2cSlave.cpp
+++ examples/i2cSlave.cpp
@@ -63,12 +63,12 @@ bool testRX =true;
     i=1;
     uint8_t data[16]; //Make an array of 16 bytes to be sure that the buffer fits
 	//memcpy(&xfer.txBuf,&data,2);
-    int status_return = bscXfer(&xfer);//See what happened:
+    int status_return = bscXfer(&xfer);//See what happened
     int rxBufSize = xfer.rxCnt;//Read how much there is to be read in rx buff
     if(rxBufSize>0){
         std::cout << "There are " << rxBufSize << " available bytes in rx buffer...";
         memcpy(&data,&xfer.rxBuf,rxBufSize);
-        std::cout << "Read." << std::endl;
+        std::cout << "Read." << data << std::endl;
     }else{
         std::cout << "No available data in rx buf." << std::endl;
     }
