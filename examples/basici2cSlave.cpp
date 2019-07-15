74c2fbe69726de81b6787304159958af021f48c4
Andersound <fa8@kth.se>
2019-07-15 09:58:12 +0200

    Added cout of  recieved floats

--- examples/basici2cSlave.cpp
+++ examples/basici2cSlave.cpp
@@ -40,15 +40,21 @@ int main(int argc, char** argv){
    	 }
     }
     //initialize i2c slave object with the inherited encode/decode class
-const int slaveAddress = 0x03;
+    const int slaveAddress = 0x03;
     robustpositioning::i2cSlave_decode i2cComm(slaveAddress);
     std::vector<float> values;
+    std::vector<float> valuesTX{1.3,2.5,0.8};
     while(1){
-        if(i2cComm.readAndDecodeBuffer(values)>0){
-            std::cout << "Recieved " << i2cComm.RxBufferContentSize() <<"decoded floats" << std::endl;
+        int recievedFloats = i2cComm.readAndDecodeBuffer(values);
+        if(recievedFloats>0){
+            std::cout << "Recieved " << recievedFloats <<"decoded floats" << std::endl;
+            for(float i:values){
+                std::cout << i << ", ";
+            }
+            std::cout << std::endl;
         }else{std::cout << "No available data in rx buffer" << std::endl;}
         usleep(3000000);
-        int writtenBytes = i2cComm.writeAndEncodeBuffer(values);
+        int writtenBytes = i2cComm.writeAndEncodeBuffer(valuesTX);
         if(writtenBytes>0){
             std::cout << "Wrote " << writtenBytes << " bytes to tx buffer" << std::endl;
         }else{ std::cout << "Could not write to tx buffer " << std::endl;}
