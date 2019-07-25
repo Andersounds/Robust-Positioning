9b50b022bc1b0f97bddace48e9be766200971b80
Andersound <fa8@kth.se>
2019-07-25 09:35:02 +0200

    Added function prototypes for unDistort (barrel distortion) and angulation class attributes of the distortion parameters k1,k2,k3

--- src/angulation.hpp
+++ src/angulation.hpp
@@ -18,6 +18,9 @@ namespace ang{
         cv::Mat_<float> K_inv;                  //Inverted Camera matrix
         cv::Mat_<float> T;                      //uav to camera transformation matrix
         cv::Mat_<float> T_inv;                  //Inverted uav to camera transformation matrix. (=camera to uav in x-y-z order)
+        float k1_barrel;                        //Distortion coefficients for barrel distortion
+        float k2_barrel;
+        float k3_barrel;
         int maxId;                              //Max size of database. Specified upon initiation
         int minAnchors;                         //Minimum amount of anchors to try angulation
         std::vector<bool> activeAnchors;        //Part of database specifying what IDs are known
@@ -29,8 +32,10 @@ namespace ang{
         angulation(int,std::string);
         void setKmat(cv::Mat_<float>);          //Camera matrix
         void setTmat(cv::Mat_<float>);          //uav -to- camera frame transformation matrix
+        void setDistortCoefficents(float,float,float);//Set k1,k2,k3 distortion coefficients
         int addAnchor(int,cv::Mat_<float>);     //Add an anchor to database (id and 3d coordinate)
         int calculate(std::vector<cv::Mat_<float>>&, std::vector<cv::Mat_<float>>&,std::vector<bool>&,cv::Mat_<float>&,float&,float,float); //Take pixel coordinates and IDs of anchors and calculate AZIPE
+        void unDistort(std::vector<cv::Point2f>&);//Compensate for barrel distortion
         void pix2uLOS(const std::vector<cv::Point2f>&,std::vector<cv::Mat_<float>>&);//Converts image pixel coordinates to uLOS vectors in uav frame
         void pix2uLOS(const std::vector<std::vector<cv::Point2f>>&,std::vector<cv::Mat_<float>>&);//Overloaded version for corner locations of anchors instead of center locations
         int dataBase2q(const std::vector<int>&,std::vector<cv::Mat_<float>>&,std::vector<bool>&);//Retrieves Q vectors from database given the IDs
