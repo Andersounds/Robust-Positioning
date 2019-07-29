36e8e299edf05e3736aba152da9a68b547d05175
Andersound <fa8@kth.se>
2019-07-29 12:56:11 +0200

    Some changes

--- examples/positioning.cpp
+++ examples/positioning.cpp
@@ -56,13 +56,12 @@ if(!databin_LOG.init("5_jul/truePath.csv",std::vector<std::string>{"Timestamp [m
     //Initialize positioning object
     int maxIdAruco = 50;
     std::string anchorPath = S.data.anchorPath;//"anchors.txt";
-    int flowGrid = 4;
-    cv::Rect2f roiSize(245,125,150,150);
+    int flowGrid = S.data.optical_flow_grid;
+    cv::Rect2f roiSize = S.data.ROI;
     cv::Mat_<float> K = S.data.K;
-
     cv::Mat_<float> T = S.data.T;//warper.getZRot(-PI/2);//UAV frame is x forward, camera frame is -y forward
-    pos::positioning P(pos::OF_MODE_KLT,
-                        pos::VO_MODE_AFFINE,
+    pos::positioning P(pos::OF_MODE_CORR,//pos::OF_MODE_CORR,//pos::OF_MODE_KLT,
+                        pos::VO_MODE_AFFINE,//pos::VO_MODE_HOMOGRAPHY,//pos::VO_MODE_AFFINE,
                         cv::aruco::DICT_4X4_50,
                         maxIdAruco,anchorPath,flowGrid,roiSize,K,T);
     //Init values of position and yaw
@@ -80,9 +79,9 @@ float timeStamp_image;
 int counter = 0;
     while(getData.get(data)){
         timeStamp_data = data[0];
-        float height = -data[3];//This is used as a subst as actual height is not in dataset
-        float pitch = data[5];
-        float roll = data[6];
+        float height = data[1];//This is used as a subst as actual height is not in dataset
+        float pitch = data[2];
+        float roll = data[3];
 
 //Get new image
         if(VStreamer.peek()<=timeStamp_data){
@@ -91,13 +90,14 @@ int counter = 0;
             cv::cvtColor(frame, colorFrame, cv::COLOR_GRAY2BGR);
             int mode = P.processAndIllustrate(pos::MODE_AZIPE_AND_VO,frame,colorFrame,pos::ILLUSTRATE_ALL,height,roll,pitch,yaw,t);
             cv::imshow("showit",colorFrame);
-            //cv::waitKey(0);
+            cv::waitKey(0);
             if( cv::waitKey(1) == 27 ) {std::cout << "Bryter"<< std::endl;return 1;}
 
 std::cout << "Lap " << counter << std::endl;
       }
 counter++;
 
+    std::cout << "t: " << t.t() << std::endl;
         //Maybe convert to color for illustartion?
         //cv::cvtColor(colorFrame, frame, cv::COLOR_BGR2GRAY);
 
