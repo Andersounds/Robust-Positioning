a512ef7badcb2bb242c8ebcfef47a9b858ba26c0
Andersound <fa8@kth.se>
2019-08-15 13:34:19 +0200

    Initial commit of L Marton robust technique

---
+++ src/martonRobust.hpp
@@ -0,0 +1,18 @@
+#ifndef MARTONROBUST_H
+#define MARTONROBUST_H
+/*
+    This class implements a version of the robust trilateration method presented by L Marton et al (isbn: 9781509025916)
+    Adaptation is done to fit the use of angulation instead of lateration
+    Adaptions include:
+        - Angulation to visible anchor point is used together with a separate height estimation
+            to find expression for possible circle in 3d
+        - The final position estimation is used together with initial angulation data to estimate yaw angle
+*/
+
+
+
+
+
+
+
+#endif
