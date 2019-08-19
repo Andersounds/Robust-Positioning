8a905cda0a3a5911a1cf159b88a4fd4e62ae1f3c
Andersound <fa8@kth.se>
2019-08-19 12:19:44 +0200

    Changed basic i2c example to only recieve

--- examples/basici2cSlave.cpp
+++ examples/basici2cSlave.cpp
@@ -42,23 +42,24 @@ int main(int argc, char** argv){
     //initialize i2c slave object with the inherited encode/decode class
     const int slaveAddress = 0x04;
     robustpositioning::i2cSlave_decode i2cComm(slaveAddress);
-    std::vector<float> values;
+    std::vector<float> values{0,0,0,0};
     std::vector<float> valuesTX{1.3,2.5,0.8};
     while(1){
         int recievedFloats = i2cComm.readAndDecodeBuffer(values);
         if(recievedFloats>0){
-            std::cout << "Recieved " << recievedFloats <<"decoded floats" << std::endl;
+            std::cout << "Recieved " << recievedFloats <<" decoded floats" << std::endl;
             for(float i:values){
                 std::cout << i << ", ";
             }
             std::cout << std::endl;
         }else{std::cout << "No available data in rx buffer" << std::endl;}
-        usleep(3000000);
+        /*usleep(3000000);
         int writtenBytes = i2cComm.writeAndEncodeBuffer(valuesTX);
         if(writtenBytes>0){
             std::cout << "Wrote " << writtenBytes << " bytes to tx buffer" << std::endl;
         }else{ std::cout << "Could not write to tx buffer " << std::endl;
                 std::cout << "Tx buf content size: " <<i2cComm.TxBufferContentSize() << std::endl;}
+        */
         usleep(3000000);
     }
 
