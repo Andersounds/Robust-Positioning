2b43b116549aec395b2690fffbcd648dcfbd4813
Andersound <fa8@kth.se>
2019-08-26 13:29:48 +0200

    Added a settings struct for cleaner initialization. Added option to skip logging

--- examples/dataCollection_rpi.cpp
+++ examples/dataCollection_rpi.cpp
@@ -19,12 +19,25 @@
 #include <unistd.h> //For sleep
 #define PI 3.1416
 
+
+struct dataCollSettings{
+    std::string basePath = "";
+    std::string newDirName = "";
+    std::string fileEnding = ".bmp";
+    std::string csvDataPath = "imudata.csv";
+    int logData = 1;
+    int waitI2c = 1;
+    int FpsThrottlingms = 0;
+    int laps = 100;
+
+};
+
 /*
 -p relative path to directory where new directory is to be saved (optional. if skipped then it is created in pwd)
 -d new directory name
 -f wanted filename of the data logger
 */
-int parsePaths(std::vector<std::string>& paths,std::vector<int>& params_,int argc, char** argv){
+int parsePaths(dataCollSettings& settings,int argc, char** argv){
     std::string basePath = "";
     std::string newDirName = "";
     std::string csvDataName = "imudata.csv";
@@ -44,12 +57,15 @@ int parsePaths(std::vector<std::string>& paths,std::vector<int>& params_,int arg
                 //Make sure that dir ends with /
                 if(std::regex_match(arg,std::regex("(.*)(/)"))){
                     basePath = arg;
+                    settings.basePath = arg;
                 }else{
                     basePath = arg + "/";
+                    settings.basePath = arg + "/";
                 }
                 std::cout << "Set base path to: " << basePath << std::endl;
             }else if(flag == "-d"){
                 newDirName = arg;
+                settings.newDirName = arg;
                 gotDirName = true;
                 std::cout << "Set dir name to: " << newDirName << std::endl;
             }else if(flag == "-f"){
@@ -58,44 +74,42 @@ int parsePaths(std::vector<std::string>& paths,std::vector<int>& params_,int arg
                     csvDataName = arg;
                 }else{
                     csvDataName = arg + ".csv";
+                    settings.csvDataName = arg + ".csv";
                 }
                 std::cout << "Set file name to: " << csvDataName << std::endl;
             }else if(flag == "-l"){
                 try{
                     laps = stoi(arg);
+                    settings.laps = stoi(arg);
                 } catch(const std::invalid_argument& ia){
                     std::cout << "Gave invalid number of laps to run (specified with flag -l): " << arg << std::endl;
                 }
                 std::cout << "Set number of laps to: " << laps << std::endl;
-            }else if(flag == "-cp1"){
-                try{
-                    cp1 = stoi(arg);
-                    cp1Set = true;
-                } catch(const std::invalid_argument& ia){
-                    std::cout << "Gave invalid int for cp1: " << arg << std::endl;
-                }
-            }else if(flag == "-cp2"){
-                try{
-                    cp2 = stoi(arg);
-                    if(cp1Set){
-                        params_.push_back(cp1);
-                        params_.push_back(cp2);
-                        cp1Set = false;
-                    }else{
-                        std::cout << "cp2 set without specifying cp1.." << std::endl;
-                    }
-                } catch(const std::invalid_argument& ia){
-                    std::cout << "Gave invalid int for cp2: " << arg << std::endl;
-                }
-            }else if(flag == "-cp3"){
-                fileEnding = arg;
             }else if(flag == "-w"){
-	        try{
+	            try{
                     waitSeconds = stoi(arg);
                 } catch(const std::invalid_argument& ia){
                     std::cout << "Gave invalid int for -w wait [s]: " << arg << std::endl;
                 }
-   	    }
+   	        }else if(flag == "-t"){
+                try{
+                    settings.FpsThrottlingms = stoi(arg);
+                } catch(const std::invalid_argument& ia){
+                    std::cout << "Gave invalid int for -t (FPS throttling): " << arg << std::endl;
+                }
+            }else if(flag == "-i2c"){
+                try{
+                    settings.waitI2c = stoi(arg);
+                } catch(const std::invalid_argument& ia){
+                    std::cout << "Gave invalid int for -i2c (wait to resend i2c): " << arg << std::endl;
+                }
+            }else if(flag == "-log"){
+                try{
+                    settings.logData = stoi(arg);
+                } catch(const std::invalid_argument& ia){
+                    std::cout << "Gave invalid int for -log (log data): " << arg << std::endl;
+                }
+            }
         }
     }
     if(!gotDirName){
@@ -105,10 +119,10 @@ int parsePaths(std::vector<std::string>& paths,std::vector<int>& params_,int arg
         std::cout << "-f   optional    Name of csv file in which i2c data is to be saved. default imudata.csv" << std::endl;
         std::cout << "-p   optional    Relative path to directory in which new dir is to be created. Default to pwd. \"\" gives top of hierarchy"  << std::endl;
         std::cout << "-l   optional    Number of laps to do. default to 100." << std::endl;
-        std::cout << "-cp1 optional    imwrite compression parameter 1 (name) " << std::endl;
-        std::cout << "-cp2 optional    imwrite compression parameter 1 (name) " << std::endl;
-        std::cout << "-cp3 optional    imwrite file ending" << std::endl;
 	    std::cout << "-w   optional    wait before starting data collection [s]" << std::endl;
+        std::cout << "-t   optional    Framerate throttling (ms/lap) (default 0)" << std::endl;
+        std::cout << "-i2c optional    enable (default:1), disable(0) wait for i2c." << std::endl;
+        std::cout << "-log optional    enable (default:1), disable(0) logging" << std::endl;
         return 0;
     }else{
         std::cout << "Logging to directory " << basePath << newDirName << std::endl;
@@ -117,7 +131,9 @@ int parsePaths(std::vector<std::string>& paths,std::vector<int>& params_,int arg
         paths.push_back(basePath);
         paths.push_back(newDirName);
         paths.push_back(basePath + newDirName +"/" + csvDataName);
+        settings.csvDataPath = basePath + newDirName +"/" + csvDataName;
         paths.push_back(fileEnding);
+        settings.fileEnding = fileEnding;
         if(waitSeconds>0){
 	    std::cout << "Waiting " << waitSeconds << " seconds..." << std::endl;
 	    while(waitSeconds>0){
@@ -136,19 +152,19 @@ int main(int argc, char** argv){
     timestamp::timeStamp_ms stamp;
     std::vector<std::string> paths;
     std::vector<int> imWriteParams;
-    int laps = parsePaths(paths,imWriteParams,argc,argv);
+    dataCollSettings Sett;
+    int laps = parsePaths(Sett,argc,argv);
     if(!laps){return 0;}
     //Initialize imagebin. It automatically creates a directory 'images' in the given path
     robustPositioning::imageLogger imagebin;
-    imagebin.init(paths[0],paths[1]);
+    imagebin.init(Sett.basePath,Sett.newDirName);
     //Initialize databin
     robustPositioning::dataLogger databin_LOG;
-    if(!databin_LOG.init(paths[2],std::vector<std::string>{"Timestamp [ms]","dist [m]","height [m]","pitch [rad]","roll [rad]","watchdog [ms]"})) return 0;
+    if(!databin_LOG.init(Sett.csvDataPath,std::vector<std::string>{"Timestamp [ms]","dist [m]","height [m]","pitch [rad]","roll [rad]","watchdog [ms]"})) return 0;
 
     //Initialize video stream
     robustPositioning::Streamer VStreamer(robustPositioning::MODE_RPI_CAM,CV_8UC1);
-    imagebin.params = imWriteParams;
-    imagebin.imgFormatStr = paths[3];
+    imagebin.imgFormatStr = Sett.fileEnding;
     cv::Mat frame;
     //Initialize data stream
     //initialize i2c slave object with the inherited encode/decode class
@@ -167,38 +183,40 @@ int main(int argc, char** argv){
     float counter = 0;
     double timeStamp; //
     stamp.get(timeStamp);//Initialize to get start value
-    while(counter<laps){
+    while(counter<Sett.laps){
         i2cComm.clearRxBuffer();                            //Clear data so that new can be recieved
         stamp.get(timeStamp_data);                          //Set data timestamp
         stamp.get(timeStamp_image);                         //Set image timestamp
         VStreamer.getImage(frame);			    //Get image
-        imagebin.dump(timeStamp_image,frame);		    //Log image (By doing this now we give some extra time  for i2c)
+        if(Sett.logData){
+            imagebin.dump(timeStamp_image,frame);		    //Log image (By doing this now we give some extra time  for i2c)
+        }
     // Read i2c message
         float watchdog=0;//Wait maximal 0.5s on imu data
         int recv_amount = i2cComm.readAndDecodeBuffer(data);;//Number of recieved and decoded floats
-        while(recv_amount<0 && watchdog<10){          //Try to read until we get the requested data. max 1/10 s
+        while(recv_amount<0 && watchdog<10 && Sett.waitI2c){          //Try to read until we get the requested data. max 1/10 s
 	        usleep(2000);//Wait 2 ms
 	        watchdog++;
 	        recv_amount = i2cComm.readAndDecodeBuffer(data);
         }
 	//Only update the variables that  have been recieved. And do not try to access outside bounds of data vector.
-	switch(recv_amount){
-	    case 5:batt = data[4];
-	    case 4:height = -data[3];
-	    case 3:dist = data[2];
-	    case 2:roll = data[1];
-	    case 1:{pitch = data[0];
-			std::cout <<counter <<". WD: "<< watchdog << ". Recieved " << recv_amount <<" decoded floats. roll: "<< roll<< ", batt: " << batt << std::endl;
-			break;}
-	    case 0:{std::cout << "No available data in rx buffer" << std::endl;break;}
-	    default:{std::cout << "Recieved more than 5 decoded floats? " << recv_amount << std::endl;}
-	}
+    	switch(recv_amount){
+    	    case 5:batt = data[4];
+    	    case 4:height = -data[3];
+    	    case 3:dist = data[2];
+    	    case 2:roll = data[1];
+    	    case 1:{pitch = data[0];
+    			std::cout <<counter <<". WD: "<< watchdog << ". Recieved " << recv_amount <<" decoded floats. roll: "<< roll<< ", batt: " << batt << std::endl;
+    			break;}
+    	    case 0:{std::cout << "No available data in rx buffer" << std::endl;break;}
+    	    default:{std::cout << "Recieved more than 5 decoded floats? " << recv_amount << std::endl;}
+	    }
         //Log data
-	//    std::cout << "Roll: " << roll << ", pitch: " << pitch << std::endl;
-        //std::cout << "Watchdog: " << watchdog << " [ms]" << std::endl;
-        std::vector<float> logData{timeStamp_data, dist, height,pitch, roll,watchdog};
-        databin_LOG.dump(logData);
-        counter++;
+        if(Sett.logData){
+            std::vector<float> logData{timeStamp_data, dist, height,pitch, roll,watchdog};
+            databin_LOG.dump(logData);
+            counter++;
+        }
     }
     stamp.get(timeStamp_data);
     float total_time_s = (timeStamp_data-timeStamp)/1000;
