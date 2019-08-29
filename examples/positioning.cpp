a3f4296c651eae3d15e4e84c52bb78fa221d61b4
Andersound <fa8@kth.se>
2019-08-29 22:27:58 +0200

    Some changes to figure out bug

--- examples/positioning.cpp
+++ examples/positioning.cpp
@@ -51,6 +51,9 @@ if(!databin_LOG.init("5_jul/truePath.csv",std::vector<std::string>{"Timestamp [m
     int skipLines = 1;
     robustPositioning::dataStreamer getData(dataFile,skipLines);
     std::vector<float> data;
+    //Initialize data logger
+    robustPositioning::dataLogger databin_LOG;
+    if(!databin_LOG.init("log.csv",std::vector<std::string>{"timestamp [ms]","X [m]","Y [m]","Z [m]","Yaw [rad]"})) return 0;
 
 
     //Initialize positioning object
@@ -91,12 +94,17 @@ float rad2Grad = 57.2958;
             VStreamer.getImage(frame);
             if(frame.empty()){std::cout << "Video stream done."<< std::endl; return 0;}
 //            int mode = P.process(pos::MODE_AZIPE_AND_VO,frame,height, roll, pitch, yaw, t);
-//            int mode = P.process(pos::MODE_AZIPE_AND_VO,frame,height, roll, pitch, yaw, t);
+            int mode = P.process(pos::MODE_AZIPE,frame,dist, roll, pitch, yaw, t);
             cv::cvtColor(frame, colorFrame, cv::COLOR_GRAY2BGR);
-            int mode = P.processAndIllustrate(pos::MODE_AZIPE_AND_VO,frame,colorFrame,pos::ILLUSTRATE_ALL,dist,roll,pitch,yaw,t);
-            std::cout << "roll: " << roll*rad2Grad<<", pitch: " << pitch*rad2Grad <<std::endl;
+            //int mode = P.processAndIllustrate(pos::MODE_AZIPE_AND_VO,frame,colorFrame,pos::ILLUSTRATE_ALL,dist,roll,pitch,yaw,t);
+            //Log data
+            if(true){
+                std::vector<float> logData{timeStamp_data,t(0,0),t(1,0),t(2,0),yaw};
+                databin_LOG.dump(logData);
+            }
+            std::cout << "X: "<< t(0,0) << ", Y: "<< t(1,0) << ", Z: " << t(2,0) <<", roll: " << roll*rad2Grad<<", pitch: " << pitch*rad2Grad << "yaw: " << yaw<< std::endl;
             cv::imshow("showit",colorFrame);
-            cv::waitKey(0);
+            //cv::waitKey(0);
             if( cv::waitKey(1) == 27 ) {std::cout << "Bryter"<< std::endl;return 1;}
 
             std::cout << "Lap " << counter << std::endl;
