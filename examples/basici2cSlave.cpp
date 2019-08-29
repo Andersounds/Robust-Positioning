3b0fa3e984bbd07447787529f159f5a737009b55
Fred_Rpi <fa8@kth.se>
2019-08-29 08:47:42 +0100

    Changed so that program couts pitch and roll for data collection

--- examples/basici2cSlave.cpp
+++ examples/basici2cSlave.cpp
@@ -1,10 +1,13 @@
 #include <pigpio.h>
 #include <iostream>
 #include <unistd.h> //For sleep
+#include "../src/timeStamp.cpp" //For timestamp
 #include "../src/i2c_slave.cpp"
 
 
 int main(int argc, char** argv){
+timestamp::timeStamp_ms stamp; //initialize timestamper
+float timestamp_data = 0;
 // Parse argument
     int lapCounter = 0;
     int lapMax = 0;
@@ -32,17 +35,20 @@ int main(int argc, char** argv){
     while(lapMax < 0 || lapCounter<lapMax){
 	lapCounter++;
 	i2cComm.clearRxBuffer();
+	stamp.get(timestamp_data); //Get new timestamp
 	usleep(25000);//wait approx 25 ms to simulate image collect and log
 	int watchdog = 0;
 	int recv_amount = i2cComm.readAndDecodeBuffer(values);//-1;
 	while(recv_amount<0 && watchdog < 500){
+	    stamp.get(timestamp_data);//Get new timestamp
 	    usleep(1000);
 	    recv_amount = i2cComm.readAndDecodeBuffer(values);
 	    watchdog ++;
 	}
 	if(recv_amount>0){
-            std::cout <<lapCounter <<". WD: "<< watchdog << ". Recieved " << recv_amount <<" decoded floats. roll: "<< values[1]<< ", batt: " << values[4] << std::endl;
-        }else{std::cout << "No available data in rx buffer" << std::endl;}
+            //std::cout <<lapCounter <<". WD: "<< watchdog << ". Recieved " << recv_amount <<" decoded floats. roll: "<< values[1]<< ", batt: " << values[4] << std::endl;
+        	std::cout << timestamp_data << ", " << values[0] << ", " << values[1] << std::endl;
+	}else{std::cout << "No available data in rx buffer" << std::endl;}
         usleep(3000);//Pause a bit more to simulate data logging
         /* int writtenBytes = i2cComm.writeAndEncodeBuffer(valuesTX);
         if(writtenBytes>0){
