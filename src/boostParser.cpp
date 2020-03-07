90a9cc32e39be61c65e18c8d4103351d1e5ed08f
Andersound <fa8@kth.se>
2020-03-07 16:49:01 +0100

    First commit of new settins parser implementation

---
+++ src/boostParser.cpp
@@ -0,0 +1,14 @@
+/*
+This is an implementation of a settingsparser using the program_options module of the Boost-library.
+It is intended to replace the settingsParser.cpp implementation.
+*/
+
+
+
+namespace set2{
+
+
+
+
+    
+}
