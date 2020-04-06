f52deec0247efd2177d27fbb6fb6895aabc4c9d6
Andersound <fa8@kth.se>
2020-04-06 12:33:46 +0200

    Removed supporting functions that are already defined in angulation class.

--- src/martonRobust.cpp
+++ src/martonRobust.cpp
@@ -1,109 +1,42 @@
 #include <iostream>
-#include <opencv2/opencv.hpp>
-// Libraries for gsl nonlinear least squared optimization
-//#include <gsl/gsl_rng.h>
-//#include <gsl/gsl_randist.h>
-//#include <gsl/gsl_matrix.h> // Already defined in header
-//#include <gsl/gsl_vector.h> // Already defined in header
-//#include <gsl/gsl_blas.h>
-//#include <gsl/gsl_multifit_nlinear.h> // Already defined in header
-
-#include <math.h>       /* cos, pow */
 #include "martonRobust.hpp"
+
+/*
+Methods for defining the input arguments are defined in the ang::angulation class
+    src/angulation.hpp
+    src/angulation.cpp
+*/
+
+
 
 
 /* This is the main process method that handles the estimation
- *      q: q vectors (anchor coordinates), of which the first known one as indicated by mask will be used.
- *   mask: Mask that indicates which anchor to use
- * angles: alpha, beta, gamma angles as calculated from pix2angles
- * heightEst: estimated z coordinate in global system. Etimation read from atsam
- * roll, pitch: measured roll and pitch of uav.
- * outputs:
- * yaw:     yaw angle of uav estimation
- * pos:     global coordinate of uav
+ * Only first of the known anchors will be used in first implementation
+ * vector<Mat> v        A set of unit-Line-of-Sight vectors in the vehicle frame (measured) to each of the visible anchors
+ * vector<Mat> q        A corresponding set of 3d-coordinates of each anchor in the global frame
+ * float    roll   A value representing the roll of the vehicle as related to the global frame expressed in radians.
+ * float   pitch   A value representing the pitch of the vehicle as related to the global frame expressed in radians.
+
  */
-//void robustPositioning::martonRobust::process(const std::vector<cv::Mat_<float>>& q,const std::vector<bool>& mask,const std::vector<float>& angles,float heightEst, float roll, float pitch, float& yaw, cv::Mat_<float>& pos){
+//void robustPositioning::martonRobust::process(const std::vector<cv::Mat_<float>>& q,const std::vector<bool>& mask,const std::vector<float>& angles, float roll, float pitch, float& yaw, cv::Mat_<float>& pos){
 
     //Derotate (just subtract roll and pitch)
     //
 
 //}
+
+/*
+Keep same interface as azipe. v,q,pos,yaw,pitch,roll
+Possibly keep list of previous positions here. or keep thet externally and give as argument?
+
+*/
 void robustPositioning::martonRobust::process(void){
+
 
     int status = nlinear_lsqr_solve_2deg();
     std::cout << "Status: " << status << std::endl;
 }
 
-
-/* This method takes one or more pixel locations, undistorts them, and calculate alpha, beta, and gamma angles
-    alpha: apparent roll angle of uav if anchor were located directly below it and uav has 0 yaw angle
-    beta : apparent pitch angle of uav if anchor were located directly below it and uav has 0 yaw angle
-    gamma: apparent yaw of uav if anchor were located in direction of 0 yaw of uav if it has no roll or pitch
-
-    These angles can then be de-rotated using the known current roll and pitch, and the single combined remaining angle
-     is then used together with height estimation to estimate horizontal radial distance to anchor.
-*/
-void robustPositioning::martonRobust::pix2angles(const std::vector<cv::Point2f>& cornerLocations,std::vector<cv::Mat_<float>>& angles){
-
-}
-
-
-/*Overloaded version of pix2uLOS. If corner locations of anchors are given instead of points directly, the mean of each vector<point2f> is considered
- *  as the anchors location. This is calculated and then the original pix2uLOS is called
- */
-void robustPositioning::martonRobust::pix2angles(const std::vector<std::vector<cv::Point2f>>& cornerLocations,std::vector<cv::Mat_<float>>& angles){
-    std::vector<cv::Point2f> anchorLocations;
-    for(int i=0;i<cornerLocations.size();i++){//Go through all anchors
-        std::vector<cv::Point2f>::const_iterator cornerIt = cornerLocations[i].begin();
-        cv::Point2f location(0,0);
-        while(cornerIt != cornerLocations[i].end()){
-            location += *cornerIt;
-            cornerIt++;
-        }
-        location.x /= cornerLocations[i].size();
-        location.y /= cornerLocations[i].size();
-        anchorLocations.push_back(location);
-    }
-    //Do standard function call to calculate
-    return pix2angles(anchorLocations,angles);
-}
-
-
-/* method to perform in-place correction of barrel distortion of pixel coordinates
- * Using the following formula: https://docs.opencv.org/3.4.0/d4/d94/tutorial_camera_calibration.html
- */
-cv::Point2f robustPositioning::martonRobust::unDistort(const cv::Point2f& point){
-    //Shift coordinate system center to middle of image
-    float x = point.x - K(0,2);
-    float y = point.y - K(1,2);
-    //Perform undistortion
-    float r2 = x*x + y+y;//Radius squared
-    float A = (1 + k1_barrel*r2 + k2_barrel*r2*r2 + k3_barrel*r2*r2*r2);
-    cv::Point2f undistortedPoint(x/A,y/A);//SHOULD IT BE MULTIPLIED? docs are not definitive. Different in diferent version of docs
-    //Shift coordinates back to image
-    undistortedPoint.x += K(0,2);
-    undistortedPoint.y += K(1,2);
-    return undistortedPoint;
-}
-/* Interface mathod used to set K mat and calculate its inverse
- */
-void robustPositioning::martonRobust::setKmat(cv::Mat_<float>K_){
-    K = K_;
-    K_inv = K.inv();
-}
-/* Interface method used to set T mat. T is transformation fro  uav frame to camera frame
- */
-void robustPositioning::martonRobust::setTmat(cv::Mat_<float> T_){
-    T = T_;
-    T_inv = T.inv();
-}
-/* Interface method to set distortion coefficients for barrel distortion compensation
-*/
-void robustPositioning::martonRobust::setDistortCoefficents(float k1, float k2, float k3){
-    k1_barrel = k1;
-    k2_barrel = k2;
-    k3_barrel = k3;
-}
 
 
 /*
@@ -175,12 +108,6 @@ for (size_t i = 0;i < 3; i++){
     double v_est_y = sin_*alpha[3] + cos_*alpha[4];
     double f13 = pow(x_est/est_norm  + v_est_x/v_norm,(double)2) + pow(y_est/est_norm + v_est_y/v_norm,(double)2);
     gsl_vector_set (f, 12, f13);
-
-    for(size_t i = 0;i<12;i++){
-        double cost = gsl_vector_get (f, i);
-        std::cout << cost << ", ";
-    }
-    std::cout << std::endl;
     return GSL_SUCCESS;
 }
 
@@ -234,8 +161,8 @@ int robustPositioning::martonRobust::nlinear_lsqr_solve_2deg(void){
     //int status, info;
     //size_t i;
 
-    const double xtol = 1e-8;
-    const double gtol = 1e-8;
+    const double xtol = 1e-2;
+    const double gtol = 1e-2;
     const double ftol = 0.0;
 
     //gsl_rng_env_setup();
--- src/martonRobust.hpp
+++ src/martonRobust.hpp
@@ -4,9 +4,11 @@
     This class implements a version of the robust trilateration method presented by L Marton et al (isbn: 9781509025916)
     Adaptation is done to fit the use of angulation instead of lateration
     Adaptions include:
-        - Angulation to visible anchor point is used together with a separate height estimation
-            to find expression for possible circle in 3d
-        - The final position estimation is used together with initial angulation data to estimate yaw angle
+        - Sensor measurement is 3d unit vector pointing towards anchor instead if single ToF measurement
+        - Polynomial estimations are not only for x and y, but for z and yaw as well
+        - The last equality of the cost function is adapted to
+            - 3d via cone surface equation instead of 2d circle at known z
+            - Another equality for yaw estimation
 */
 
 #include <opencv2/opencv.hpp>
@@ -15,6 +17,7 @@
 #include <gsl/gsl_vector.h>
 #include <gsl/gsl_multifit_nlinear.h>
 
+#include <math.h>       /* cos, pow */
 
 
 namespace robustPositioning{
@@ -22,22 +25,9 @@ namespace robustPositioning{
 
 */
 class martonRobust{
-        cv::Mat_<float> K;                      //Camera matrix
-        cv::Mat_<float> K_inv;                  //Inverted Camera matrix
-        cv::Mat_<float> T;                      //uav to camera transformation matrix
-        cv::Mat_<float> T_inv;                  //Inverted uav to camera transformation matrix. (=camera to uav in x-y-z order)
-        float k1_barrel;                        //Distortion coefficients for barrel distortion
-        float k2_barrel;
-        float k3_barrel;
         int polyParameters;//How many parameters? ie what degree should the polynomial be?
         int bufferSize; //How many previous locations are to be saved in circular buffer? (min value dep on polyParameters)
     public:
-        void setKmat(cv::Mat_<float>);          //Camera matrix
-        void setTmat(cv::Mat_<float>);          //uav -to- camera frame transformation matrix
-        void setDistortCoefficents(float,float,float);//Set k1,k2,k3 distortion coefficients
-        cv::Point2f unDistort(const cv::Point2f&);//Compensate for barrel distortion
-        void pix2angles(const std::vector<cv::Point2f>&,std::vector<cv::Mat_<float>>&);//Converts image pixel coordinate(s) to apparent angles in x,y direction and around z
-        void pix2angles(const std::vector<std::vector<cv::Point2f>>&,std::vector<cv::Mat_<float>>&);//Overloaded version for corner locations of anchors instead of center location
         void process(void);// template for complete process method. add arguments and return type when clear
     private:
         int nlinear_lsqr_solve_2deg(void);     //Perform the nonlinear least square optimization Add arguments when known
