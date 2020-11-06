7270f21198a8f2536ce48cdfb75561beeeb43285
Andersound <fa8@kth.se>
2020-11-06 08:35:07 +0100

    Added Z plot

--- eval/Scripts/plotLatestEst.m
+++ eval/Scripts/plotLatestEst.m
@@ -4,9 +4,12 @@ clear all
 %d_est_path = '/Users/Fredrik/Datasets/20-04-09/';
 NUMBRS = flip([1,2,3,4,6,7,8,9,10,11,12,13,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29]);
 NUMBRS = [18,23,27,28]
-NUMBRS = [28]
+NUMBRS = [18]
 for NUM=NUMBRS
-d_est_file = strcat('/Users/Fredrik/Google Drive/Kurser/Thesis/Evaluation/20-04-09/20-04-09-',num2str(NUM),'/AZIPE_outFile.csv');
+d_est_file = strcat('/Users/Fredrik/Google Drive/Kurser/Thesis/Evaluation/20-11-3-sim/20-04-09-',num2str(NUM),'/AZIPE_outFile.csv');
+d_est_file = strcat('/Users/Fredrik/Google Drive/Kurser/Thesis/Evaluation/20-11-3-sim/20-04-09-',num2str(NUM),'/GT.csv');
+
+%d_est_file = strcat('/Users/Fredrik/Google Drive/Kurser/Thesis/Evaluation/20-04-09/20-04-09-',num2str(NUM),'/AZIPE_outFile.csv');
 %d_est_file = strcat('/Users/Fredrik/Datasets/20-04-09/20-04-09-',num2str(NUM),'/VO_outFile.csv');
 d_est= csvread(d_est_file,1,0);
    
@@ -19,22 +22,25 @@ d_est= csvread(d_est_file,1,0);
     yaw_est = d_est(:,7);
 
      figure
-    subplot(4,1,1);
+    subplot(5,1,1);
         plot(t_est,roll_est,'.');
         hold on
         plot(t_est,pitch_est,'.');
         title('Roll/pitch estimation');
         legend('Roll','Pitch');
         %axis([0,80,-2.5,2.5]);
-    subplot(4,1,2);
+    subplot(5,1,2);
         plot(t_est,x_est,'.');
         title('X direction');
-    subplot(4,1,3);
+    subplot(5,1,3);
         plot(t_est,y_est,'.');
         title('Y direction');
-    subplot(4,1,4);
+    subplot(5,1,4);
         plot(t_est,-z_est,'.');
         title('Z direction');
+    subplot(5,1,5);
+        plot(t_est,yaw_est,'.');
+        title('YAW');
 
     figure
         plot(x_est,y_est);
